fbc33e08a6b8cd51c5304ea24c0b88e628b7b6b9
Shiningsun00 <164600043+Shiningsun00@users.noreply.github.com>
2024-09-25 06:35:56 +0000

    qwe

--- heachan/cpp_object_class/week3.cpp
+++ heachan/cpp_object_class/week3.cpp
@@ -3,10 +3,30 @@
 int lab(int n) {
     return n*n;
 }
-
-int main(){
+void practice1(void){
     int n = 0;
     std::cin >> n;
     std::cout << "제곱근 : " << lab(n) <<std::endl;
+}
+
+void myPrint(int a) {
+    std::cout << "정수 출력 : " << a <<std::endl;
+}
+
+void myPrint(double a) {
+    std::cout << "실수 출력 : " << a <<std::endl;
+}
+
+void myPrint(std::string a) {
+    std::cout << "문자 출력 : " << a <<std::endl;
+}
+
+
+
+
+int main() { 
+    myPrint(10);
+    myPrint(10.05);
+    myPrint('정수도');
     return 0;
 }
